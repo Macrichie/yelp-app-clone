3d1a952678ab240af176d2a8b60c93efd1c83afc
Macrichie <makanjuolakunle@gmail.com>
2020-12-09 00:33:35 +0000

    Added sql queries to create tables

--- db/db.sql
+++ db/db.sql
@@ -1,3 +1,11 @@
+CREATE TABLE IF NOT EXISTS restaurants (
+    id BIGINT NOT NULL PRIMARY KEY,
+    name VARCHAR(50) NOT NULL,
+    location VARCHAR(50) NOT NULL,
+    price_range INT check(price_range >=1 and price_range <= 5)
+);
+
+
 CREATE TABLE IF NOT EXISTS reviews (
     id BIGSERIAL NOT NULL PRIMARY KEY,
     restaurant_id BIGINT NOT NULL REFERENCES restaurants(id),
